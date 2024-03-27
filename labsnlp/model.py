f1f3434b864b2177d9e9900498ae95ef1ba2cf98
Світлана Жук <svitlana.zhuk@stud.onu.edu.ua>
2024-03-27 15:09:18 +0200

    tree min_samples_split=10,max_depth=3, ROC-AUC: 0.6581250000000001, time!

--- labsnlp/model.py
+++ labsnlp/model.py
@@ -1,8 +1,7 @@
 from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 
 def train_gaussian_nb_classifier(X, y):
@@ -16,12 +15,7 @@ def train_native_bayes_classifier(X, y):
     return clf
 
 def train_logistic_regression(X, y):
-    clf = LogisticRegression(max_iter=1000)
-    clf.fit(X,y)
-    return clf
-
-def train_SGD_classifier(X, y):
-    clf = SGDClassifier()
+    clf = LogisticRegression(max_iter=100)
     clf.fit(X,y)
     return clf
 
@@ -30,12 +24,7 @@ def train_neighbors_classifier(X, y):
     clf.fit(X,y)
     return clf
 
-def train_svm_classifier(X, y):
-    clf = LinearSVC(C=0.0001)
-    clf.fit(X,y)
-    return clf
-
 def train_tree_classifier(X, y):
-    clf = DecisionTreeClassifier(min_samples_split=10,max_depth=3)
+    clf = DecisionTreeClassifier(min_samples_split=10,max_depth=30)
     clf.fit(X,y)
     return clf
